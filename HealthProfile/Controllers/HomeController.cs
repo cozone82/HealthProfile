6dff9c1ecaf30c426dbbc04147e70e42307accb3
Cory Townes <Cory Townes@DESKTOP-KCOJ1DS>
2022-05-17 23:55:19 -0400

    Add project files.

---
+++ HealthProfile/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using HealthProfile.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthProfile.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Health model)
+        {
+            ViewBag.FullName = model.FullName();
+            ViewBag.Age = model.Age();
+            ViewBag.Gender = model.Gender;
+            ViewBag.BMI = model.BMI();
+            ViewBag.MaximumHeartRate = model.MaximumHeartRate();
+            ViewBag.MinimumTargetHeartRate = model.MinimumTargetHeartRate();
+            ViewBag.MaximumTargetHeartRate = model.MaximumTargetHeartRate();
+
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HealthProfile/Models/Health.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HealthProfile.Models
+{
+    public class Health
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int BirthYear { get; set; }
+        public int CurrentYear { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        public string FullName()
+        {
+            string fullname = FirstName + " " + LastName;
+            return fullname;
+        }
+        public int Age()
+        {
+            int age = CurrentYear - BirthYear;
+            return age;
+        }
+
+        public int MaximumHeartRate()
+        {
+            int maxHeartRate = 220 - (CurrentYear - BirthYear);
+            return maxHeartRate;
+        }
+
+        public double MinimumTargetHeartRate()
+        {
+            double minTargetHeartRate = .5 * MaximumHeartRate();
+            return minTargetHeartRate;
+        }
+
+        public double MaximumTargetHeartRate()
+        {
+            double maxTargetHeartRate = .85 * MaximumHeartRate();
+            return maxTargetHeartRate;
+        }
+
+        public double BMI()
+        {
+            double weightHeight = (Weight * 703) / (Height * Height);
+            double truncated = Math.Truncate(weightHeight * 100) / 100; 
+            return truncated;
+        }
+
+    }
+}
